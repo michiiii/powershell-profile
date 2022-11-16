e3549fc1ed9f1117bef587cfabdfc54a39ec3f91
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2022-11-17 00:37:52 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -14,8 +14,7 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
                 New-Item -Path ($env:userprofile + "\Documents\WindowsPowerShell") -ItemType "directory"
             }
         }
-
-        Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+        Invoke-RestMethod https://raw.githubusercontent.com/l4rm4nd/powershell-profile/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
         Write-Host "The profile @ [$PROFILE] has been created."
     }
     catch {
